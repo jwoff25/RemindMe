60f03de73aee5b6e5bf98949e4e23aa09a5c4e00
Jeremy <jeremywoffinden25@gmail.com>
2017-10-27 13:47:03 -1000

    added add/subtract functions and button functionalities. tried to add dialog, only works once.

--- app/src/main/java/com/example/jeremy/testapp/SubActivity.java
+++ app/src/main/java/com/example/jeremy/testapp/SubActivity.java
@@ -1,29 +1,40 @@
 package com.example.jeremy.testapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.R.id.message;
 
 /**
  * Created by Jeremy on 10/13/2017.
  */
 
 public class SubActivity extends AppCompatActivity {
+    //FIELD VARIABLES
     Button addButton;
     Button subButton;
+    Button settingsButton;
     TextView countView;
+    EditText input;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
 
+        input = new EditText(getApplicationContext());
+
         addButton = (Button)findViewById(R.id.addButton);
         subButton = (Button)findViewById(R.id.minusButton);
+        settingsButton = (Button)findViewById(R.id.configButton);
         countView = (TextView)findViewById(R.id.counterNumber);
-
-        int bob = 2;
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,17 +48,43 @@ public class SubActivity extends AppCompatActivity {
                 subtract();
             }
         });
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
     }
 
+    //SUBTRACT
     public void subtract(){
         int count = Integer.parseInt(countView.getText().toString());
         count--;
         countView.setText(String.valueOf(count));
     }
 
+    //ADD
     public void add(){
         int count = Integer.parseInt(countView.getText().toString());
         count++;
         countView.setText(String.valueOf(count));
+    }
+
+    //ONLY WORKS ONCE LOL
+    public void openDialog(){
+       AlertDialog alert = new AlertDialog.Builder(this)
+                .setTitle("Update Counter")
+                .setMessage("Set value of counter.")
+                .setView(input)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value = input.getText().toString();
+                        countView.setText(value);
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Do nothing.
+            }
+        }).show();
     }
 }
