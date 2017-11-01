b48050f3d741d4b4be96eb38e29e8261c5185bcb
Jeremy <jeremywoffinden25@gmail.com>
2017-10-31 22:54:15 -1000

    added options class

---
+++ app/src/main/java/com/example/jeremy/testapp/Options.java
@@ -0,0 +1,12 @@
+package com.example.jeremy.testapp;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by Jeremy on 10/31/2017.
+ */
+
+public class Options extends PreferenceActivity {
+
+
+}
